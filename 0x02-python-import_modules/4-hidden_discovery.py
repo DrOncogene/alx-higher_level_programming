a36be54922c4e66169fc98818cdd3167d967a0d8
Nasir Abdulrasheed <mypythtesting@gmail.com>
2021-11-18 15:29:07 +0100

    using the dir builtin

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    names = dir("hidden_4.pyc")
+    import hidden_4 as hidden
+    names = dir(hidden)
     for name in names:
         if name[:2] != "__":
             print(name)
