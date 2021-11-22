d6b87a0ca076b104ebf6d5ed0de877af7529b61a
Nasir Abdulrasheed <mypythtesting@gmail.com>
2021-11-22 18:45:18 +0100

    adds two tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,11 +2,13 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     '''adds two tuples'''
     if len(tuple_a) == 1:
-        tuple_a[1] = 0
+        tuple_c = tuple_a[0], 0
+        tuple_a = tuple_c
     elif len(tuple_a) == 0:
         tuple_a = 0, 0
     if len(tuple_b) == 1:
-        tuple_b[1] = 0
+        tuple_c = tuple_b[0], 0
+        tuple_b = tuple_c
     elif len(tuple_b) == 0:
         tuple_b = 0, 0
     a, b = tuple_a[0], tuple_a[1]
