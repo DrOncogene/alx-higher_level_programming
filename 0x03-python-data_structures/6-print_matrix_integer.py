8ffd3b916da0a55fedd3636862f6579dc7abc7ad
Nasir Abdulrasheed <mypythtesting@gmail.com>
2021-11-22 17:12:26 +0100

    prints a matrix of int

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    '''prints a matrix'''
+    for item in matrix:
+        if type(item) == list:
+            for i in item:
+                if item.index(i) == len(item) - 1:
+                    print("{:d}".format(i))
+                else:
+                    print("{:d} ".format(i), end='')
+    print("--")
