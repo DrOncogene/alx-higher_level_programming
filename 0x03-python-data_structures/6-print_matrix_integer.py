0027c162e182b5a4164e58a51ae759f2ee0f5664
Nasir Abdulrasheed <mypythtesting@gmail.com>
2021-11-23 22:59:45 +0100

    handled empty strings

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     '''prints a matrix'''
-    if len(matrix) == 0:
-        print("")
     for item in matrix:
         if type(item) == list:
+            if len(item) == 0:
+                print("")
             for i in item:
                 if item.index(i) == len(item) - 1:
                     print("{:d}".format(i))
