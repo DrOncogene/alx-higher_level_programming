03942a750331d3a3f1c0c84a0aec666c30578a8f
Nasir Abdulrasheed <mypythtesting@gmail.com>
2021-11-23 21:39:40 +0100

    python/c api to print info about lists

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,18 +1,15 @@
 #include <Python.h>
 #include <stdio.h>
 
-void
-print_python_list_info(PyObject *p)
+void print_python_list_info(PyObject *p)
 {
 	long long i, size;
-	PyObject *item;
 	PyListObject *the_list = (PyListObject *)p;
 
 	size = PyList_Size(p);
 	printf("[*] Size of the python list = %lld\n", size);
 	printf("[*] Allocated = %lld\n", (long long)the_list->allocated);
 	for (i = 0; i < size; i++){
-		item = PyList_GetItem(p, i);
-		printf("Element %lld: %s\n", i, Py_TYPE(item)->tp_name);
+		printf("Element %lld: %s\n", i, Py_TYPE(the_list->ob_item[i])->tp_name);
 	}
 }
