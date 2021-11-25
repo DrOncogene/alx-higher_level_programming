82311b0476af83d8c9b2db6af0d192de21e7e4ea
Nasir Abdulrasheed <mypythtesting@gmail.com>
2021-11-25 10:58:10 +0100

    finds the highest value in a dict

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,15 +4,5 @@ def best_score(a_dict):
         return None
     scores = [item[1] for item in a_dict.items()]
     keys = [item[0] for item in a_dict.items()]
-    best = scores[0]
-    for score in scores:
-        index = scores.index(score)
-        if index + 1 < len(scores):
-            best = check(best, score)
+    best = max(scores)
     return keys[scores.index(best)]
-
-
-def check(a, b):
-    if a > b:
-        return a
-    return b
