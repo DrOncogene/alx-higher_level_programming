5f86b4d4300626f37f229f3fbac9f5b0061e3455
Nasir Abdulrasheed <mypythtesting@gmail.com>
2021-12-07 00:59:16 +0100

    divides a list by another

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def list_division(list_1, list_2, list_len):
+    i, res_list = 0, []
+    while i < list_len:
+        try:
+            res = list_1[i] / list_2[i]
+        except (ValueError, TypeError):
+            res = 0
+            print("wrong type")
+        except ZeroDivisionError:
+            res = 0
+            print("division by 0")
+        except IndexError:
+            res = 0
+            print("out of range")
+        i += 1
+        res_list.append(res)
+    return res_list
