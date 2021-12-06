916ad4f1bfeebf43553353ae43d8061023cfb6b2
Nasir Abdulrasheed <mypythtesting@gmail.com>
2021-12-06 20:11:05 +0100

    prints a specified number of items from a list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     try:
-        for i in range(x):
+        i = 0
+        while i < x:
             print("{0}".format(my_list[i]), end='')
+            i += 1
         print("\n", end='')
-        return x
+        return i
     except IndexError:
         print("\n", end='')
-        return x
+        return i
