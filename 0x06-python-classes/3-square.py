fb14eaad028c60b9ca5a4cbfed911ed804f62efc
Nasir Abdulrasheed <mypythtesting@gmail.com>
2021-12-08 11:34:55 +0100

    added a public area method to the class

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''A module that defines and manipulates square objects'''
+
+
+class Square:
+    '''Defines a square '''
+
+    def __init__(self: object, size=0) -> object:
+        '''The class constructor
+        Args:
+            size: the size of the square
+        '''
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+        def area(self):
+            '''Calculates the area of the square
+            Args:
+                none
+            Return:
+                int: area of the square
+            '''
+            return self.__size ** 2
