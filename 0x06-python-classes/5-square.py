59446bbe6619b8c6546909db811ed5d6c00e8912
Nasir Abdulrasheed <mypythtesting@gmail.com>
2021-12-08 18:00:53 +0100

    added a print function to the class

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+'''A module that defines and manipulates square objects'''
+
+
+class Square:
+    '''Defines a square '''
+
+    def __init__(self: object, size=0) -> object:
+        '''The class constructor
+        Args:
+            size(int): the size of the square
+        '''
+        self.size = size
+
+    def area(self):
+        '''Calculates the area of the square
+        Args:
+            none
+        Return:
+            int: area of the square
+        '''
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        '''size getter
+        Args:
+            none
+        Return:
+            int: size of the square
+        '''
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        '''size setter
+        Args:
+            value(int): the size
+        Return:
+            int: size of the square
+        '''
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        '''prints the square with #'''
+        if self.__size == 0:
+            print("\n", end='')
+            return
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print("#", end='')
+            print("\n", end='')
