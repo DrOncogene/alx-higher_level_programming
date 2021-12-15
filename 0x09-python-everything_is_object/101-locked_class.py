4fc4d5a97851587307143b4238b52759642d1b94
Nasir Abdulrasheed <mypythtesting@gmail.com>
2021-12-15 12:58:28 +0100

    rewrote it

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -7,9 +7,3 @@ class LockedClass:
     '''defines an object that cannot be dynamically allocated
     new attributes'''
     __slots__ = ['first_name']
-    '''def __setattr__(self, attr, value):
-        if attr == 'first_name':
-            self.__dict__[attr] = value
-        else:
-            raise AttributeError("'{}' object has no \
-attribute '{}'".format(self.__class__.__name__, attr))'''
