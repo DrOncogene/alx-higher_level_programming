b701cb575ce9d8848928a1c1c739da8c7d15dc4f
Nasir Abdulrasheed <mypythtesting@gmail.com>
2021-12-23 20:39:43 +0100

    prints some statics from data read from stdin

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -9,28 +9,30 @@ def print_stats(counts_dict, size):
     print(f"File size: {size}")
     for k, v in counts_dict.items():
         if v > 0:
-            print(f"{k}: {v}", end='')
+            print(f"{k}: {v}")
 
 
 status_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
-lines = 0
 counts = dict()
 for status in status_codes:
     counts[status] = 0
 
 size = 0
 numInterrupt = 0
+lines = 0
 while True:
     try:
-        for i in range(10):
-            line = sys.stdin.readline()
-            if len(line) == 0:
-                break
-            line_list = line.split(" ")
-            status = line_list[-2]
-            size += int(line_list[-1])
-            counts[status] += 1
-        print_stats(counts, size)
+        line = sys.stdin.readline()
+        if len(line) == 0 and lines < 10:
+            print_stats(counts, size)
+            break
+        line_list = line.split(" ")
+        status = line_list[-2]
+        size += int(line_list[-1])
+        counts[status] += 1
+        lines += 1
+        if lines % 10 == 0:
+            print_stats(counts, size)
     except KeyboardInterrupt as e:
         print_stats(counts, size)
         sleep(2)
