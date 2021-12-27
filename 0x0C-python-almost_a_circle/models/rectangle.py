a4e9437f85cda93babb302f128e40ddc977ababf
Nasir Abdulrasheed <mypythtesting@gmail.com>
2021-12-27 21:03:17 +0100

    modified the display func to incoorporate x and y

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -64,7 +64,11 @@ class Rectangle(Base):
 
     def display(self):
         '''displays the rectangle using #'''
+        for i in range(self.y):
+            print('')
         for i in range(self.height):
+            for k in range(self.x):
+                print(" ", end='')
             for j in range(self.width):
                 print("#", end='')
             print('')
