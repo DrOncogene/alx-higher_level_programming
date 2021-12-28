45bc1d4bd28e13bee422b287a38d6d0a7ae45081
Nasir Abdulrasheed <mypythtesting@gmail.com>
2021-12-28 20:25:53 +0100

    added a method to convert a dict to json string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 '''module that defines a base shapes class'''
+import json
 
 
 class Base:
@@ -12,3 +13,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dict):
+        '''returns a json string of list_dict'''
+        if list_dict is None or len(list_dict) == 0:
+            return "[]"
+        return json.dumps(list_dict)
