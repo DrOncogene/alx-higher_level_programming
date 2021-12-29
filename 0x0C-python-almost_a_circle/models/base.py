5dc8d49c9d835a2393c6e7cf0a3c7e1c295cee72
Nasir Abdulrasheed <mypythtesting@gmail.com>
2021-12-29 19:17:13 +0100

    added csv serialization and deserialization methods to the Base class to save data to a file in csv format

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 '''module that defines a base shapes class'''
 import json
 import os
+import csv
 
 
 class Base:
@@ -40,6 +41,9 @@ class Base:
         '''save a json str of list_objs to a file'''
         filename = f"{cls.__name__}.json"
         with open(filename, "w") as f:
+            if list_objs is None or len(list_objs) == 0:
+                f.write(Base.to_json_string([]))
+                return
             list_dict = [obj.to_dictionary() for obj in list_objs]
             f.write(Base.to_json_string(list_dict))
 
@@ -68,3 +72,82 @@ class Base:
                 list_objs.append(cls.create(**obj))
 
         return list_objs
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        '''save an object to a file in csv format using the csv module'''
+        filename = f"{cls.__name__}.csv"
+        with open(filename, "w") as csvfile:
+            if cls.__name__ == "Rectangle":
+                attr_list = ['id', 'width', 'height', 'x', 'y']
+            elif cls.__name__ == "Square":
+                attr_list = ['id', 'size', 'x', 'y']
+            csv_writer = csv.DictWriter(csvfile, fieldnames=attr_list)
+            csv_writer.writeheader()
+            if list_objs is None or len(list_objs) == 0:
+                csv_writer.writerow({})
+                return
+            obj_list = [obj.to_dictionary() for obj in list_objs]
+            for obj in obj_list:
+                csv_writer.writerow(obj)
+
+    @classmethod
+    def load_from_file_csv(cls) -> list:
+        '''returns a list of objects recreated from a csv file'''
+        filename = f"{cls.__name__}.csv"
+        if not os.path.exists(filename):
+            return []
+
+        list_objs = []
+        with open(filename, "r") as f:
+            objs = csv.DictReader(f)
+            for obj in objs:
+                for attr, value in obj.items():
+                    obj[attr] = int(value)
+                list_objs.append(cls.create(**obj))
+
+        return list_objs
+
+# alternate forms of the above two funcs implemented
+# without using the csv module
+#    @classmethod
+#    def save_to_file_csv(cls, list_objs):
+#        '''save an object to a file in csv format'''
+#        filename = f"{cls.__name__}.csv"
+#        with open(filename, "w") as f:
+#            if list_objs is None or len(list_objs) == 0:
+#                f.write('')
+#                return
+#            obj_list = [obj.to_dictionary() for obj in list_objs]
+#            for obj in obj_list:
+#                obj_str = ''
+#                if cls.__name__ == "Square":
+#                    obj_str += f"{obj['id']},{obj['size']},"
+#                elif cls.__name__ == "Rectangle":
+#                    obj_str += f"{obj['id']},{obj['width']},{obj['height']},"
+#                obj_str += f"{obj['x']},{obj['y']}\n"
+#                f.write(obj_str)
+
+#    @classmethod
+#    def load_from_file_csv(cls) -> list:
+#        '''returns a list of objects recreated from a csv file'''
+#        filename = f"{cls.__name__}.csv"
+#        if not os.path.exists(filename):
+#            return []
+
+#        list_objs = []
+#        with open(filename, "r") as f:
+#            content = f.readlines()
+#            for line in content:
+#                obj = {}
+#                attr_list = line.split(',')
+#                obj['id'] = int(attr_list[0])
+#                obj['x'], obj['y'] = int(attr_list[-2]), int(attr_list[-1])
+#               if cls.__name__ == "Rectangle":
+#                   obj['width'] = int(attr_list[1])
+#                   obj['height'] = int(attr_list[2])
+#                elif cls.__name__ == "Square":
+#                    obj['size'] = int(attr_list[1])
+#
+#               list_objs.append(cls.create(**obj))
+#       return list_objs
