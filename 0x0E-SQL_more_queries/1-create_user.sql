4ab9091afb055f3fd20ee38f1e535a14c2a0c935
Nasir Abdulrasheed <mypythtesting@gmail.com>
2022-02-03 23:52:32 +0100

    added the if not exists clause

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,6 @@
 -- creates a new user
-CREATE USER 'user_0d_1'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
+ IDENTIFIED BY 'user_0d_1_pwd';
 -- grants all permissions to all databases
 GRANT ALL
    ON *.*
