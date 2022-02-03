bbdebe322b698c3d7f1d79e375d1f2e9fb1441c9
Nasir Abdulrasheed <mypythtesting@gmail.com>
2022-02-04 00:39:38 +0100

    mtchewwwwwww

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,7 +1,9 @@
 -- creates a database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- use the database
+USE hbtn_0d_usa;
 -- create a new table
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+CREATE TABLE IF NOT EXISTS states (
 	PRIMARY KEY(id),
-	id		INT	AUTO INCREMENT	NOT NULL,
+	id		INT	NOT NULL AUTO_INCREMENT,
 	name 	VARCHAR(256)	NOT NULL );
