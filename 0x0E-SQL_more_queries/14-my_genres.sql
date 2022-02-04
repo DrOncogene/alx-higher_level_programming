776410292f612806483c64e296276c1ca58bcf0e
Nasir Abdulrasheed <mypythtesting@gmail.com>
2022-02-04 22:28:40 +0100

    using double joins

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -2,6 +2,6 @@
 SELECT DISTINCT tv_genres.name
   FROM tv_genres INNER JOIN tv_show_genres AS genres LEFT JOIN tv_shows
     ON tv_genres.id = genres.genre_id
- WHERE tv_shows.title = 'Dexter'
+ WHERE tv_shows.title = "Dexter"
 
  ORDER BY tv_genres.name;
