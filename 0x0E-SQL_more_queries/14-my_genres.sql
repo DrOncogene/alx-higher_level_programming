7e8fd1fc660967be2b443d5cde38a3302415bd3c
Nasir Abdulrasheed <mypythtesting@gmail.com>
2022-02-04 22:27:44 +0100

    using join with a subquery

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,10 +1,7 @@
 -- list genres for show Dexter
 SELECT DISTINCT tv_genres.name
-  FROM tv_genres INNER JOIN tv_show_genres AS genres
+  FROM tv_genres INNER JOIN tv_show_genres AS genres LEFT JOIN tv_shows
     ON tv_genres.id = genres.genre_id
- WHERE genres.show_id = (
-		 SELECT id
-		   FROM tv_shows
-		  WHERE title = 'Dexter')
+ WHERE tv_shows.title = 'Dexter'
 
  ORDER BY tv_genres.name;
