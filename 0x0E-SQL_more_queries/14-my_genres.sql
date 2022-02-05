18331eca0fefab693c6033ee360cc363ced9e960
Nasir Abdulrasheed <mypythtesting@gmail.com>
2022-02-05 07:10:55 +0100

    using multiple joins

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,7 +1,9 @@
 -- list genres for show Dexter
 SELECT DISTINCT tv_genres.name
-  FROM tv_genres INNER JOIN tv_show_genres AS genres LEFT JOIN tv_shows
-    ON tv_genres.id = genres.genre_id
- WHERE tv_shows.title = "Dexter"
+  FROM tv_genres
+  	INNER JOIN tv_show_genres AS genres 
+	INNER JOIN tv_shows AS shows
+    ON tv_genres.id = genres.genre_id AND shows.id = genres.show_id
+  WHERE shows.title = "Dexter"
 
  ORDER BY tv_genres.name;
