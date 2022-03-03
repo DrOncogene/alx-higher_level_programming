1f05fcaa7a7c7f876ca276729c1a0c3e6c0cfade
Nasir Abdulrasheed <mypythtesting@gmail.com>
2022-03-03 19:58:31 +0100

    using the map() function

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const list = require('./100-data.js').list;
-const newList = list.map((item) => {
-  return item;
+const newList = list.map((item, index) => {
+  return item * index;
 });
 console.log(list);
 console.log(newList);
