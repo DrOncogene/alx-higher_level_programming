0759314eb722cbdabfbd5bd6a9bd2e7e22327507
Rasheed Nasir <mypythtesting@gmail.com>
2022-03-06 23:28:43 +0100

    lists all cities belonging to a given state

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""script to select all cities in a given state"""
+import MySQLdb as sqldb
+from sys import argv as sysargv
+
+
+def get_state_cities():
+    """selects all cities from the state passed as an argument"""
+    script, user, passwd, db_name, state = sysargv
+    db = sqldb.connect(host='localhost', port=3306,
+                       user=user, passwd=passwd, db=db_name)
+    c = db.cursor()
+    query = "SELECT cities.name FROM cities\
+             WHERE cities.state_id=(\
+             SELECT id FROM states\
+             WHERE name='{}')\
+             ORDER BY cities.id".format(state)
+    c.execute(query)
+    states = c.fetchall()
+    for state in states:
+        print(state[0], end='')
+        if states.index(state) != len(states) - 1:
+            print(", ", end='')
+    print('')
+
+
+if __name__ == "__main__":
+    get_state_cities()
