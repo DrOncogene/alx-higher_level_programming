c4f6766d0e890ceeb636fff2db2576dfa8279bc2
Nasir Abdulrasheed <mypythtesting@gmail.com>
2022-03-06 08:31:38 +0100

    using the LIKE clause to match patterns

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""script to select all entries from the states table from a db
+passed as an argument"""
+import MySQLdb as sqldb
+from sys import argv as sysargv
+
+
+def get_states():
+    """selects all states from the database"""
+    script, user, passwd, db_name = sysargv
+    db = sqldb.connect(host='localhost', port=3306,
+                       user=user, passwd=passwd, db=db_name)
+    c = db.cursor()
+    c.execute("""SELECT * FROM states
+              WHERE (name LIKE 'N%')
+               ORDER BY states.id""")
+    states = c.fetchall()
+    for state in states:
+        print(state)
+
+
+if __name__ == "__main__":
+    get_states()
