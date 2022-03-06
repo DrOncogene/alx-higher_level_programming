b2c19c5f5be562ad22c0562f0180fe88f54cbe01
Rasheed Nasir <mypythtesting@gmail.com>
2022-03-06 15:22:33 +0100

    minor changes

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""script to select all entries from the states table from a db
+"""script to select matching entries from the states table from a db
 passed as an argument"""
 import MySQLdb as sqldb
 from sys import argv as sysargv
 
 
 def get_state():
-    """selects all states from the database that starts with N"""
+    """selects all states from the database that matches a given state"""
     script, user, passwd, db_name, state = sysargv
     db = sqldb.connect(host='localhost', port=3306,
                        user=user, passwd=passwd, db=db_name)
