367c67b365cdf5e6f445f3e18921b0816ed1c4dd
Rasheed Nasir <mypythtesting@gmail.com>
2022-03-06 20:30:12 +0100

    adding an SQL injection preventing code

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""script to select matching entries from the states table from a db
+passed as an argument"""
+import MySQLdb as sqldb
+from sys import argv as sysargv
+
+
+def get_state():
+    """selects all states from the database that matches a given state"""
+    script, user, passwd, db_name, state = sysargv
+    db = sqldb.connect(host='localhost', port=3306,
+                       user=user, passwd=passwd, db=db_name)
+    c = db.cursor()
+    state = state.replace("'", "").replace('"', '').replace("OR", '')
+    query = f"SELECT * FROM states\
+             WHERE states.name='{state}'\
+             ORDER BY states.id"
+    c.execute(query)
+    states = c.fetchall()
+    for state in states:
+        print(state)
+
+
+if __name__ == "__main__":
+    get_state()
