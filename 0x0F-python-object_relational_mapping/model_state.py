6812ab504ec4db5371775fdc8824295ece8c4eb6
Rasheed Nasir <mypythtesting@gmail.com>
2022-03-07 19:14:19 +0100

    added the reverse relationship

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -2,6 +2,7 @@
 """Defines a State as the ORM for states table"""
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 
 
 Base = declarative_base()
@@ -14,3 +15,4 @@ class State(Base):
     id = Column('id', Integer, primary_key=True,
                 unique=True, nullable=False)
     name = Column('name', String(128), nullable=False)
+    cities = relationship("City", back_populates='state')
