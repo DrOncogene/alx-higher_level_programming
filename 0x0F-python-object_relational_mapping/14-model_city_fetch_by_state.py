e621b68bbe57e7e19cc75288d300986a9ac5faa7
Rasheed Nasir <mypythtesting@gmail.com>
2022-03-07 18:50:47 +0100

    a City class to represent city objects, linked to the states table

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""script to delete states with a in their name"""
+from model_state import Base
+from model_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, relationship
+import sys
+
+
+def get_cities():
+    """deletes states with a in their name from the database"""
+    user, passwd, db = sys.argv[1], sys.argv[2], sys.argv[3]
+    engine = create_engine(f"mysql+mysqldb://{user}:\
+                           {passwd}@localhost:3306/{db}")
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    cities = session.query(City).all()
+    for city in cities:
+        print(f"{city.state.name}: ({city.id}) {city.name}")
+
+    session.close()
+
+
+if __name__ == "__main__":
+    get_cities()
---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Defines a City class as the ORM for the cities table"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from model_state import Base
+
+
+class City(Base):
+    """mapping class for the states table"""
+    __tablename__ = 'cities'
+
+    id = Column('id', Integer, primary_key=True,
+                unique=True, nullable=False)
+    name = Column('name', String(128), nullable=False)
+    state_id = Column('state_id', Integer, ForeignKey('states.id'),
+                      nullable=False)
+    state = relationship("State", back_populates='cities')
