594468cc0145c20ece7afaad828c1627bbc74aef
Rasheed Nasir <mypythtesting@gmail.com>
2022-03-07 10:36:58 +0100

    using sqlalchemy to retrieve all states from the database

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -5,14 +5,20 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import sys
 
-user, passwd, db = sys.argv[1], sys.argv[2], sys.argv[3]
-engine = create_engine(f"mysql+mysqldb://{user}:\
-                        {passwd}@localhost:3306/{db}")
-Base.metadata.create_all(engine)
-Session = sessionmaker(bind=engine)
-session = Session()
-states = session.query(State)
-for state in states:
-    print(f"{state.id}: {state.name}")
 
-session.close()
+def get_states():
+    user, passwd, db = sys.argv[1], sys.argv[2], sys.argv[3]
+    engine = create_engine(f"mysql+mysqldb://{user}:\
+                           {passwd}@localhost:3306/{db}")
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).order_by(State.id)
+    for state in states:
+        print(f"{state.id}: {state.name}")
+
+    session.close()
+
+
+if __name__ == "__main__":
+    get_states()
