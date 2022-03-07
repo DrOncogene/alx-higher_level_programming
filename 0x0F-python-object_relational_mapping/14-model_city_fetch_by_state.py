224c55b41caefa8caba776ac170c624863389fc7
Rasheed Nasir <mypythtesting@gmail.com>
2022-03-07 18:56:24 +0100

    added the ordering clause

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -15,7 +15,7 @@ def get_cities():
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    cities = session.query(City).all()
+    cities = session.query(City).order_by(City.id).all()
     for city in cities:
         print(f"{city.state.name}: ({city.id}) {city.name}")
 
