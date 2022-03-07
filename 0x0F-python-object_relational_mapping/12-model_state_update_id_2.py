9846d317d628e3fafe5d6f8c1b85d7208ad9551b
Rasheed Nasir <mypythtesting@gmail.com>
2022-03-07 13:33:31 +0100

    updates the name of the state with id 2

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""script to update the name of the state with id 2"""
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+
+def update_state(id: int):
+    """updates the name of the state with id 2"""
+    user, passwd, db = sys.argv[1], sys.argv[2], sys.argv[3]
+    engine = create_engine(f"mysql+mysqldb://{user}:\
+                           {passwd}@localhost:3306/{db}")
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state = session.query(State).filter(State.id == id).first()
+    state.name = 'New Mexico'
+    session.commit()
+    session.close()
+
+
+if __name__ == "__main__":
+    update_state(2)
