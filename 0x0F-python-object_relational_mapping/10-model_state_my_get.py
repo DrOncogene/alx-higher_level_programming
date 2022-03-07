3e7958cd4abe8a484dfae866ffa8d8b4786004f0
Rasheed Nasir <mypythtesting@gmail.com>
2022-03-07 13:05:43 +0100

    selects a given state from the states table

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""script to print any State object that contains 'a' from a db"""
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+
+def get_state(state: str):
+    """selects all states with 'a' in their name from states"""
+    user, passwd, db = sys.argv[1], sys.argv[2], sys.argv[3]
+    engine = create_engine(f"mysql+mysqldb://{user}:\
+                           {passwd}@localhost:3306/{db}")
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).filter(State.name == state).all()
+    for state in states:
+        print(f"{state.id}: {state.name}")
+    if len(states) == 0:
+        print('Not found')
+
+    session.close()
+
+
+if __name__ == "__main__":
+    get_state(sys.argv[4])
