3fd4e688987214d4337a4f3229a78edf09e4abfd
Rasheed Nasir <mypythtesting@gmail.com>
2022-03-08 07:49:33 +0100

    rewrote the model classes to include bidirectional one-to-many relationsip from State to City

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Defines a State as the ORM for states table"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+
+
+Base = declarative_base()
+
+
+class State(Base):
+    """mapping class for the states table"""
+    __tablename__ = 'states'
+
+    id = Column('id', Integer, primary_key=True,
+                unique=True, nullable=False)
+    name = Column('name', String(128), nullable=False)
+    cities = relationship("City", back_populates='state',
+                           cascade="all, delete, delete-orphan")
