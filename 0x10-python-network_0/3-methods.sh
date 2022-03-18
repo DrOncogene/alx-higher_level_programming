e8532155f4402d97d8cab4439888dbbdc75727b5
Rasheed Nasir <mypythtesting@gmail.com>
2022-03-19 00:17:03 +0100

    changed I to i

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # prints all http methods supported by a server
-curl -si -X OPTIONS "$1" | grep -oP 'Allow: \K\S+' | sed 's/,/, /g'
+curl -si -X OPTIONS -L "$1" | grep -oP 'Allow: \K\S+' | sed 's/,/, /g'
