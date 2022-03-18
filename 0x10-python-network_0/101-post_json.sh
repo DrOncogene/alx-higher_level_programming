a5e1c86076475f0959015ab74dffaf1a73d97f5d
Rasheed Nasir <mypythtesting@gmail.com>
2022-03-18 23:36:12 +0100

    sends a post request with data from a json file

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a data in a json file to a server
-curl -sL -H "Content-Type: application/octet-stream" --data-binary @"$2" "$1"
+curl -s -H "Content-Type: application/json" --data-binary @"$2" "$1"
