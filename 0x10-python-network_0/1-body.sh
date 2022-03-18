9759a35868060b216152be93dada715767215e9f
Nasir Rasheed <mypythtesting@gmail.com>
2022-03-18 09:44:17 +0000

    added option for redirection handling

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # prints the body of a response only if status is 200
-if [ "$(curl -s -o /dev/null -sw '%{http_code}' "$1")" -eq 200 ]; then curl -s "$1"; fi
+if [ "$(curl -sL -o /dev/null -sw '%{http_code}' "$1")" -eq 200 ]; then curl -s "$1"; fi
