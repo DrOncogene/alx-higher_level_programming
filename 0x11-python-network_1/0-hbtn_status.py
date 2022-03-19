5836b5b2327282ecff6ce161bfd492f9526d3569
Rasheed Nasir <mypythtesting@gmail.com>
2022-03-19 09:59:00 +0100

    fetches a url and prints the response body

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""fetches data from a url using urllib"""
+import urllib.request
+
+
+def get_status():
+    """fetches a resource"""
+    req = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(req) as f:
+        resp = f.read()
+        print('Body response:')
+        print('\t - type: {}'.format(type(resp)))
+        print('\t - content: {}'.format(resp))
+        print('\t - utf8 content: {}'.format(resp.decode('utf-8')))
+
+
+if __name__ == "__main__":
+    get_status()
