43f0698cd748737d17165f9f0303e90725a3db9e
Rasheed Nasir <mypythtesting@gmail.com>
2022-03-19 15:34:53 +0100

    minor modifications

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -5,15 +5,10 @@ import urllib.request
 import urllib.error
 
 
-def error_code(url: str):
-    """prints the error code following a request"""
+if __name__ == "__main__":
     req = urllib.request.Request(url)
     try:
         with urllib.request.urlopen(req) as f:
             print(f.read().decode('utf-8'))
     except urllib.error.HTTPError as e:
         print(f"Error code: {e.code}")
-
-
-if __name__ == "__main__":
-    error_code(sys.argv[1])
