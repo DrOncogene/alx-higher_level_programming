61bb0f74b361cea98e0fb6393ba32a3c47dd7631
Rasheed Nasir <mypythtesting@gmail.com>
2022-03-19 16:38:54 +0100

    using the github users api

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Script that
+  - takes Github username and PAT
+  - authenticates with github
+  - prints user id
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    user, pat = sys.argv[1], sys.argv[2]
+    res = requests.get(f"https://api.github.com/users/{user}",
+                       auth=(user, pat))
+    print(res.json().get('id'))
