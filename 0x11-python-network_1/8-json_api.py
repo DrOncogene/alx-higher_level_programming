ef4d8e78c4c1fa6fb18a3424234aa6a012e200b1
Rasheed Nasir <mypythtesting@gmail.com>
2022-03-19 15:03:27 +0100

    added more details to the module doc

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 """
-sends a post request and prints the response if
-it's a valid JSON
+sends a post request and prints the response if it's a valid JSON
+prints 'No result' or 'Not valid JSON' in some cases
+
+args:
+    letter(str): single letter
+return:
+    None
 """
 import sys
 import requests
@@ -9,7 +14,7 @@ import requests
 
 def json_api(letter: str):
     """processes a json response"""
-    res = requests.post("http://0.0.0.0:5000/search_user:", data={'q': letter})
+    res = requests.post("http://0.0.0.0:5000/search_user", data={'q': letter})
     try:
         res_dict = res.json()
         if len(res_dict) == 0:
