8dddd6826347fb7426cc99bb51de9a1fcbd292d4
Rasheed Nasir <mypythtesting@gmail.com>
2022-03-19 12:18:15 +0100

    prints the response headers

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -7,8 +7,7 @@ import requests
 def get_header(url: str):
     """prints the X-Request-Id header from the response"""
     resp = requests.get(url)
-    print(resp.headers['X-Request-Id'])
-
+    print(resp.headers.get('X-Request-Id'))
 
 if __name__ == '__main__':
     get_header(sys.argv[1])
