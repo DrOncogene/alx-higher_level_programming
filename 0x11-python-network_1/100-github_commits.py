3fa864870947344e349490fadc7fa38ca4873711
Nasir Rasheed <mypythtesting@gmail.com>
2022-03-22 10:59:31 +0000

    changed the if condition for the number of commits printed

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -14,7 +14,8 @@ if __name__ == "__main__":
     commits = res.json()
 
     for i in range(len(commits)):
-        if i < 10:
-            sha = commits[i].get('sha')
-            author_name = commits[i].get('commit').get('author').get('name')
-            print(f"{sha}: {author_name}")
+        if i >= 10:
+            break
+        sha = commits[i].get('sha')
+        author_name = commits[i].get('commit').get('author').get('name')
+        print(f"{sha}: {author_name}")
